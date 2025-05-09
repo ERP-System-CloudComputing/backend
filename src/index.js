78d9e889cb9fc39f2e66319f7b3b2140716fbbee
Mario Cobian <marvanis@hotmail.com>
2025-05-09 09:07:42 -0600

    [ADDED]: Configuracion para hacer funcional el servidor y el archivo de index de routes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('Hola desde index');
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import router from './routes/index.js'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use('/api', router)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT} 💫`);
+})
