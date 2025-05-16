7f05efcd5d46c5fa2f60051e6caf4361f8c5e8ed
Luis-Alvarezz <luisangelug10@gmail.com>
2025-05-16 12:27:56 -0600

    [ADDED]: Se añadio método asincrono con logica para login

--- src/services/StaffService.js
+++ src/services/StaffService.js
@@ -1,10 +1,36 @@
 import StaffRepository from "../repositories/StaffRepository.js";
-import Staff from "../modules/Staff.js";
+import Staff from "../models/Staff.js";
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken';
+
 export default class StaffService {
   constructor () {
     this.staffRepository = new StaffRepository()
   }
+
+  async login(personalEmail, password) {
+    const user = await this.staffRepository.findByUser(personalEmail);
+    if (!user)
+        throw { messasge: 'User not found', statusCode: 401 };
+
+    // ! Verificar si ya hay sesion activa
+    const tokenExists = await this.staffRepository.getSessionByToken(user.id)
+    if (tokenExists)
+        throw { messasge: 'You are already logged in', statusCode: 401 };
+
+    // ! Comparar la contraseña encritada
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw { messasge: 'Invalid credentials', statusCode: 401 };
+    }
+
+    const token = jwt.sign({ id: user.id, personalEmail: user.personalEmail, rol: user.rol}, process.env.JWT_SECRET ,{ expiresIn: '1h' } )
+
+    await this.staffRepository.updateSessionToken(user.id, token) //! Actualizar el token en la base de datos para el usuario en concreto
+    
+    return token;
+  }
+
 
   async create(staffData) {
     
@@ -27,6 +53,7 @@ export default class StaffService {
     const newStaff = new Staff({...staffData, password: hashedPassword}) // -Toma todo el objeto y solo cambia el password hash 
     return this.staffRepository.create({...newStaff})
   }
+
 
   async getAll() {
     return await this.staffRepository.getAll()
