620011c2cc0f43d9323edea1e0250892f6a741b8
Luis-Alvarezz <luisangelug10@gmail.com>
2025-05-16 12:36:44 -0600

    [MODIFY]: PRUEBA se implemento log para observar errores especificos

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -1,4 +1,5 @@
 export default function errorMiddleware(err,req,res,next){
    const statusCode = err.statusCode || 500
+   console.error('Error en errorMiddlware: ', err.stack)
    res.status(statusCode).json({ message: err.message || "Unexpected error"})
 }
