643ee74730dc2c99beabf14fcc216b3c373cf678
Luis-Alvarezz <luisangelug10@gmail.com>
2025-05-17 13:24:03 -0600

    [MODIFY]: Se agrego logica para manejo de tokens para almacenar le sesion en Cookie y metodo para actualizar token cada 15 min

--- src/controllers/StaffController.js
+++ src/controllers/StaffController.js
@@ -6,10 +6,58 @@ export default class StaffController{
 
   async login(req, res, next) {
     try {
-      const { personalEmail, password } = req.body
-      const token = await this.staffService.login(personalEmail, password);
-      res.json({ token });
+      const { personalEmail, password, rememberMe } = req.body
+      const { accessToken, refreshToken } = await this.staffService.login(personalEmail, password);
       
+      // * Configuracion de cookie para mantener la sesion iniciada:
+      const cookiesOptions = {
+        httpOnly: true,
+        secure: false, // * De momento false permitiendo que la cookie se pueda enviar a través de HTTP (necesario para pruebas locales sin HTTPS)
+        sameSite: 'lax' // * stric -> Evitamos que el navegador envie la cookie en peticiones cross-site
+      };
+
+      if (rememberMe) {
+        cookiesOptions.maxAge = 2 * 24 * 60 * 60 * 1000 // 2 dias en milisegundos
+      }
+
+      res.cookie('refreshToken', refreshToken, cookiesOptions);
+      res.json({ accessToken, expireIn: 20 * 60 }); // * 20 min
+      
+    } catch (error) {
+      next(error);
+    }
+  }
+  
+  async refreshToken(req, res, next) {
+    try {
+      const refreshToken = req.cookies.refreshToken || req.body.refreshToken;
+      if (!refreshToken) 
+        throw { message: 'Refresh token no proporcionado en Controlador', statusCode: 401 };
+    
+      const { accessToken, refreshToken: newRefreshToken } = await this.staffService.refreshAccessToken(refreshToken);
+
+      // * Actualizamos la cookie si existe:
+      if (req.cookie.refreshToken) {
+        res.cookie('refreshToken', newRefreshToken, {
+          httpOnly: true,
+          secure: false,
+          maxAge: 2 * 24 * 60 * 60 * 1000, // 2 dias en milisegundos
+          sameSite:'lax'
+        });
+      }
+      res.json({ accessToken, expireIn: 20 * 60 });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async logout(req, res, next) {
+    try {
+      const userId = req.user.id;
+      await this.staffService.logout(userId);
+        
+      res.clearCookie('refreshToken');
+      res.status(204).end();
     } catch (error) {
       next(error);
     }
