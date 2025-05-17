792b0aee22653f319566a3dce921e62799b735e9
Luis-Alvarezz <luisangelug10@gmail.com>
2025-05-17 13:25:27 -0600

    [MODIFY]: Se añadio logica en metodo login para generar tokens y actualizar la sesion de usuario en la db, y logica para actualizar token corto

--- src/services/StaffService.js
+++ src/services/StaffService.js
@@ -24,11 +24,49 @@ export default class StaffService {
       throw { messasge: 'Invalid credentials', statusCode: 401 };
     }
 
-    const token = jwt.sign({ id: user.id, personalEmail: user.personalEmail, rol: user.rol}, process.env.JWT_SECRET ,{ expiresIn: '1h' } )
+    // * Generamos ambos tokens.
+    const accessToken = this.generateAccessToken(user);
+    const refreshToken = this.generateRefreshToken(user);
 
-    await this.staffRepository.updateSessionToken(user.id, token) //! Actualizar el token en la base de datos para el usuario en concreto
+    await this.staffRepository.updateSessionTokens(user.id, { accessToken, refreshToken }); //! Actualizar el token en la base de datos para el usuario en concreto
     
-    return token;
+    return { accessToken, refreshToken };
+  }
+
+  // * Generar token corto 
+  generateAccessToken(user) {
+    return jwt.sign({ id: user.id, personalEmail: user.personalEmail, rol: user.rol }, process.env.JWT_ACCESS_SECRET, { expiresIn: '20m' });
+  }
+
+  // * Generar token largo
+  generateRefreshToken(user) {
+    return jwt.sign({ id: user.id, personalEmail: user.personalEmail, rol: user.rol}, process.env.JWT_REFRESH_SECRET, { expiresIn: '2d' });
+  }
+
+  async refreshAccessToken(refreshToken) {
+    try {
+      const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
+      const user = await this.staffRepository.getById(decoded.id);
+
+      if (!user || user.currentSession !== refreshToken) 
+        throw { message: 'Invalid refresh token', statusCode: 401 };
+
+      const newAccessToken = this.generateAccessToken(user);
+      const newRefreshToken = this.generateRefreshToken(user);
+
+      // * Actualizar el token en la base de datos:
+      await this.staffRepository.updateSessionTokens(user.id, { accessToken: newAccessToken, refreshToken: newRefreshToken });
+
+      return { accessToken: newAccessToken, refreshToken: newRefreshToken };
+
+    } catch (error) {
+      throw { message: 'Invalid refresh token', statusCode: 401 };
+    }
+  }
+
+  
+  async logout(userId) {
+    await this.staffRepository.updateSessionTokens(userId, { accessToken: null, refreshToken: null });
   }
 
 
