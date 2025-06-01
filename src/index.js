066ae9e1d797b8b6ab4d5c8f4033c2631431db43
Luis-Alvarezz <luisangelug10@gmail.com>
2025-06-01 09:45:07 -0600

    [MODIFY]: Se modifico implementacion de cors con su paquete especifico y no manualmente

--- src/index.js
+++ src/index.js
@@ -8,16 +8,26 @@ dotenv.config()
 
 const app = express()
 
+
+// * Middleware para permitir CORS manualmente
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', process.env.APP_URL_FRONT || 'http://localhost:3000');
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+//   });
+
+// * Middleware para permitir CORS con el paquete cors
+app.use(cors({
+    origin: process.env.APP_URL_FRONT || 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    exposedHeaders: ['Authorization'] // Importante para que el cliente pueda leer headers personalizados
+}));
+
 app.use(cookieParser())
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.APP_URL_FRONT || 'http://localhost:3000');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
-
 app.use(express.json())
 app.use('/api', router)
 app.use(errorMiddleware)
