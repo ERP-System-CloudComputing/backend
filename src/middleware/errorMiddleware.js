6a4aec5bb24f02af1fcf2b0610adf09e23c94748
Luis-Alvarezz <luisangelug10@gmail.com>
2025-06-01 21:19:16 -0600

    [MODIFY]: Se cambio logica para mejor manejo de errores mas especificos

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -1,5 +1,19 @@
-export default function errorMiddleware(err,req,res,next){
-   const statusCode = err.statusCode || 500
-   console.error('Error en errorMiddlware: ', err.stack)
-   res.status(statusCode).json({ message: err.message || "Unexpected error"})
+export default function errorMiddleware(err, req, res, next) {
+    if (!err) 
+        return res.status(500).json({ message: 'Internal Server Error', success: false});
+    
+    const statusCode = err.statusCode || 500;
+    let message = err.message || 'Internal Server Error';
+
+    //    console.error('Error en errorMiddlware: ', err.stack)
+    console.error('Error en errorMiddleware: ', {
+        message,
+        statusCode,
+        stack: err.stack,
+        type: err.isCredentialError ? 'CredentialError' : 'ServerError',
+        path: req.path
+        // fullError: JSON.stringify(err, Object.getOwnPropertyNames(err))
+    });
+
+   res.status(statusCode).json({ message: err.message || "Unexpected error", success: false });
 }
