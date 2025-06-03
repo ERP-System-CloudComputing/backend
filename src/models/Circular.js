00e4401a88817fdf96eb46bc375d0190cebfce53
Mario Cobian <marvanis@hotmail.com>
2025-06-02 20:01:24 -0600

    [MODIFY]: Se le incorporo el atributo al modelo de Circulars 'type'

--- src/models/Circular.js
+++ src/models/Circular.js
@@ -5,6 +5,7 @@ export default class Circular {
         sentFrom = '',
         sentTo = '',
         date = '',
+        type = 'Sent'
     }) {
         this.title = title;
         this.message = message;
