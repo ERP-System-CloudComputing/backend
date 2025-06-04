4d6d0069c2b3e6413d7498de738c34a3a8a04458
Mario Cobian <marvanis@hotmail.com>
2025-06-04 15:22:51 -0600

    [ADDED]: Se creo el modelo para LogisticsRequest

---
+++ src/models/LogisticsRequest.js
@@ -0,0 +1,32 @@
+export default LogisticsRequest = {
+    constructor(
+        title = '',
+        purpose = '',
+        amount = 0,
+        requestedBy = '',
+        sentTo = '',
+        status = 'PENDING',
+        dateFrom = '',
+        dateTo = '',
+        attachment = {}
+    ) {
+        this.title = title;
+        this.purpose = purpose;
+        this.amount = amount;
+        this.requestedBy = requestedBy;
+        this.sentTo = sentTo;
+        this.status = status;
+        this.dateFrom = dateFrom;
+        this.dateTo = dateTo;
+        this.attachment = attachment;
+    }
+
+    validate() {
+        if (!this.title || !this.purpose || this.amount <= 0 || !this.requestedBy || !this.sentTo) {
+            throw new Error('Invalid logistics request data');
+        }
+        if (this.dateFrom && this.dateTo && new Date(this.dateFrom) > new Date(this.dateTo)) {
+            throw new Error('Invalid date range');
+        }
+    }
+}
