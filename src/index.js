6c9d6922016e7ee33a699012b25208a84e3921d0
Mario Cobian <marvanis@hotmail.com>
2025-06-06 05:48:05 -0600

    [FIXED]: Duplicidad en json en index

--- src/index.js
+++ src/index.js
@@ -27,9 +27,6 @@ app.use(cors({
 }));
 
 app.use(cookieParser())
-app.use(express.json())
-app.use('/api', router)
-
 app.use(express.json({ limit: '0.9mb' }));
 app.use(express.urlencoded({ limit: '0.9mb', extended: true }));
 app.use('/api', router)
