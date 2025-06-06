40105de5cceb05f42b7317e7f3e9067daea59fff
Mario Cobian <marvanis@hotmail.com>
2025-06-05 21:26:46 -0600

    [ADDED]: Se creo el modelo para memo

---
+++ src/models/Memo.js
@@ -0,0 +1,44 @@
+export default class Memo {
+    constructor({
+        title = '',
+        sentFrom = '',
+        sentTo = '',
+        date = '',
+        action = '',
+        haveAttachment = '',
+        attachment = '',
+        typeAttachment = '',
+        body = '',
+    }) {
+        this.title = title;
+        this.sentFrom = sentFrom;
+        this.sentTo = sentTo;
+        this.date = date;
+        this.action = action;
+        this.haveAttachment = haveAttachment;
+        this.attachment = attachment;
+        this.typeAttachment = typeAttachment;
+        this.body = body;
+    }
+
+    validate () {
+        if (!this.title) {
+            throw new Error('Title is required');
+        }
+        if (!this.sentFrom) {
+            throw new Error('Sent From is required');
+        }
+        if (!this.sentTo) {
+            throw new Error('Sent To is required');
+        }
+        if (!this.date) {
+            throw new Error('Date is required');
+        }
+        if (!this.action) {
+            throw new Error('Action is required');
+        }
+        if (this.haveAttachment && !this.attachment) {
+            throw new Error('Attachment is required when haveAttachment is true');
+        }
+    }
+}
